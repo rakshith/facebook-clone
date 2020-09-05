b1baf1b9903b86a9ce9b427a7d45ce77b16dd238
Rakshith Raj S <matrixrak@gmail.com>
2020-09-05 12:13:30 +0530

    firebaseConfig removed

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,5 @@
 import firebase from "firebase";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA7IHvpbUmVH1HS8QAs6JctMNw5en9j04c",
-  authDomain: "facebook-clone-1d418.firebaseapp.com",
-  databaseURL: "https://facebook-clone-1d418.firebaseio.com",
-  projectId: "facebook-clone-1d418",
-  storageBucket: "facebook-clone-1d418.appspot.com",
-  messagingSenderId: "302642162634",
-  appId: "1:302642162634:web:12a93678c52e97e844fd33",
-  measurementId: "G-K67HRRWGYP",
-};
+import firebaseConfig from "./fireBaseConfig";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
